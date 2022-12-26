834359f6c5298be827e6d7f77fa75736540823dd
Jeongsang Kim <topkim.dev@gmail.com>
2022-12-26 15:45:44 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Git Config 작업
+git config core.hooksPath .githooks
+git config --local include.path ../.gitconfig
+
+# Swift Lint 설치
+brew install swiftlint
+
+# Swift Format 설치
+brew install swiftformat
